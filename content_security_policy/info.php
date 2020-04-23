69a0f4dbaf5eed610846cc659ed83638025acae0
Daniel Pimley <dan@pimley.net>
2020-04-23 22:47:07 +0100

    Added cacher conflict to info.php

--- content_security_policy/info.php
+++ content_security_policy/info.php
@@ -2,9 +2,11 @@
 return array(
     "name"          => __("Content Security Policy", "content_security_policy"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.1,
+    "version"       => 1.2,
     "description"   => __("Adds a Content-Security-Policy header to the blog.", "content_security_policy"),
     "author"        => array(
         "name"      => "Daniel Pimley",
-        "url"       => "http://pimley.net/")
+        "url"       => "http://pimley.net/"),
+    "conflicts"     => array(
+                       "cacher")
 );
