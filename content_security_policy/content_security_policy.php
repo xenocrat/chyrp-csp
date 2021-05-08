ec4cae4e032b48a081fd2fe46af7556eec525c5f
Daniel Pimley <dan@pimley.net>
2021-05-08 18:11:39 +0100

    Made styles policy stricter
    Removed unsafe-inline option for styles and implemented stylesheets_nonce trigger filter.

--- content_security_policy/content_security_policy.php
+++ content_security_policy/content_security_policy.php
@@ -7,12 +7,16 @@
 
             header("Content-Security-Policy:".
                    " default-src 'self';".
-                   " style-src 'self' 'unsafe-inline';".
+                   " style-src 'self' 'nonce-".$this->nonce."';".
                    " script-src 'self' 'nonce-".$this->nonce."';".
                    " frame-ancestors 'self'");
         }
 
         public function javascripts_nonce($nonce) {
+            return $this->nonce;
+        }
+
+        public function stylesheets_nonce($nonce) {
             return $this->nonce;
         }
     }
