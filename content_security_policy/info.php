9767f004760cb63c2f7948a605b9a919834b054d
Daniel Pimley <dan@pimley.net>
2021-05-30 17:22:52 +0100

    Improved description and bumped version number

--- content_security_policy/info.php
+++ content_security_policy/info.php
@@ -2,8 +2,8 @@
 return array(
     "name"          => __("Content Security Policy", "content_security_policy"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.2,
-    "description"   => __("Adds a Content-Security-Policy header to the blog.", "content_security_policy"),
+    "version"       => 1.3,
+    "description"   => __("Adds a strict Content-Security-Policy header to the blog.", "content_security_policy"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://pimley.net/")
