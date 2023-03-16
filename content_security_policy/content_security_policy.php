4d66bdea1e9cddf1d50e1fbabb59b3459b711b96
Daniel Pimley <dan@pimley.net>
2023-03-16 19:56:39 +0000

    Code cleaning

--- content_security_policy/content_security_policy.php
+++ content_security_policy/content_security_policy.php
@@ -5,12 +5,14 @@
         public function runtime(): void {
             $this->nonce = base64_encode(random(32));
 
-            header("Content-Security-Policy:".
-                   " default-src 'self';".
-                   " style-src 'self' 'nonce-".$this->nonce."';".
-                   " script-src 'self' 'nonce-".$this->nonce."';".
-                   " frame-ancestors 'self';".
-                   " form-action 'self';");
+            header(
+                "Content-Security-Policy:".
+                " default-src 'self';".
+                " style-src 'self' 'nonce-".$this->nonce."';".
+                " script-src 'self' 'nonce-".$this->nonce."';".
+                " frame-ancestors 'self';".
+                " form-action 'self';"
+            );
         }
 
         public function javascripts_nonce($nonce): string {
