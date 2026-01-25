8a30efd12668c702b89f10deffca726c8861452d
Daniel Pimley <dan@pimley.net>
2026-01-25 17:31:36 +0000

    Added support for hashing error style rules

--- content_security_policy/content_security_policy.php
+++ content_security_policy/content_security_policy.php
@@ -4,12 +4,18 @@
 
         public function runtime(
         ): void {
-            $this->nonce = base64_encode(random(32));
+            $this->nonce = base64_encode(
+                random(32)
+            );
+
+            $this->hash = base64_encode(
+                hash("sha256", error_style(), true)
+            );
 
             header(
                 "Content-Security-Policy:".
                 " default-src 'self';".
-                " style-src 'self' 'nonce-".$this->nonce."';".
+                " style-src 'self' 'nonce-".$this->nonce."' 'sha256-".$this->hash."';".
                 " script-src 'self' 'nonce-".$this->nonce."';".
                 " frame-ancestors 'self';".
                 " form-action 'self';"
